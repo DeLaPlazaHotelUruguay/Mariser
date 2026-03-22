44a497ac805953fdec05ca89b4315839f5dbe7ee
sgh4n <sgh4nl5@gmail.com>
2026-03-22 02:59:16 -0300

    Added code for trying accessing the input file
    Ok... So I got done overviewing the Python Tutorial, it doesn't seem like a hard to pick up language. It took me a while to get this one working, cuz I am not familiar with string formatting in python. I believe it took me around 30 minutes to get this tiny piece of code to work properly, but slowly I am getting my head around python.
    
    Although I have not used python before, I have used other programming languges before, like C#, bash, javascript, java, etc, and I believe that beyond the syntax of the specific language, my programming compass is well set. So I am confident the speed at which I am able to produce python code is going to ramp up dramatticlly in the next commits.

--- Codebase/Mariser_Script.py
+++ Codebase/Mariser_Script.py
@@ -1,1 +1,17 @@
-'' 
+import openpyxl
+import pdb
+
+Input_File_Path = 'C:\\Users\\sgh4n\\OneDrive\\Escritorio\\Mariser\\Modelado\\Input\\Mayores Contables, Modelado Spreadsheet.xlsx' # For now, this will be something static, later on it will be dynamically assigned
+Input_File = ''
+
+# pdb.set_trace()
+try:
+        with open(Input_File_Path, 'r') as Input_File:
+                 pass
+
+except IOError as Error:
+        print("Error leyendo el archivo ", Input_File_Path, ": ", Error)
+        F"Abortando programa :("
+        quit()
+
+print("Archivo ", Input_File.name, "abierto con exito!")
