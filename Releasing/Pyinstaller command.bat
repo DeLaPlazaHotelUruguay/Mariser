4f1e19ba30a87c597a6fc1b5cca4b40064de2379
sgh4n <sgh4nl5@gmail.com>
2026-04-11 20:32:34 -0300

    Added icon to the file itself

--- Releasing/Pyinstaller command.bat
+++ Releasing/Pyinstaller command.bat
@@ -2,4 +2,4 @@ set "Project_Basedir=%~dp0"
 set "Script_Path=%Project_Basedir%..\Codebase\Mariser_Script.py"
 
 
-pyinstaller --onefile --add-data "%Project_Basedir%\..\Assets\Images;Assets\Images" --noconsole %Script_Path%
+pyinstaller --onefile --add-data "%Project_Basedir%\..\Assets\Images;Assets\Images" --icon="%Project_Basedir%\..\Assets\Images\Window Icon.ico" --noconsole %Script_Path%
