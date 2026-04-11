3b05dfe9f2794ed0407ffd99d4f77477528ff7a9
sgh4n <sgh4nl5@gmail.com>
2026-04-11 19:21:23 -0300

    Basic bundling of the program into a single exe file
    So I learned to bundle the script along with its assets into a single file.
    I figured out that the exact way pyinstaller is called actually affects the functioning of the program, so I consider the way it is used to be an intergral part of the program as well, and thus, I put the way for it to be called into its own file.
    
    The exe works fine. Although there's a few details to correct. First, I wouldn't like the program to open a console behind it. There's a reason why I set up an elegant system for making the exceptions available to the programer through the GUI without disturbing the user experience. Second, I would like for the exe itself to have the program's icon; when I open the program, the icon shows fine on the taskbar, but the file itself doensn't show it. I don't know if the last one is actually possible, but I check it out.
    Update: Whoops, wrote onedir all along. Fixed

---
+++ Releasing/Pyinstaller command.bat
@@ -0,0 +1,5 @@
+set "Project_Basedir=%~dp0"
+set "Script_Path=%Project_Basedir%..\Codebase\Mariser_Script.py"
+
+
+pyinstaller --onefile --add-data "%Project_Basedir%\..\Assets\Images;Assets\Images" %Script_Path%
